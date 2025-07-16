63bc76ffe675358bda7dc4ba74061c532bed4c23
steve2700 <stevnyaru@gmail.com>
2025-07-16 16:52:39 +0200

    add the granite countertop image as the layout cover

--- app/page.tsx
+++ app/page.tsx
@@ -114,7 +114,7 @@ export default function HomePage() {
 
             <div className="relative order-1 lg:order-2">
               <Image
-                src="/kitchen-renovation-granite-installation-johannesburg.jpg?height=600&width=800"
+                src="/granite-countertop.jpg?height=600&width=800"
                 alt="Professional kitchen renovation with granite countertops by Johannesburg carpentry experts"
                 width={800}
                 height={600}
